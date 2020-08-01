dbeed1ecad86e9e3044cc4e79d352ecf2866743e
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-07-31 21:22:06 -0700

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/balanced-brackets/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+
+"""
+UPER:
+
+U:
+    > Start the problem
+"""
+
+def isBalanced(s):
+    print('is it balanced?')
+
